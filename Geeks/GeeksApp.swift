c767bbe58913a1721e37b0533ccaddbbd895d13c
jwkwon0817 <jeewon.kwon.0817@gmail.com>
2024-11-11 23:59:52 +0900

    Initial Commit

---
+++ Geeks/GeeksApp.swift
@@ -0,0 +1,17 @@
+//
+//  GeeksApp.swift
+//  Geeks
+//
+//  Created by  jwkwon0817 on 11/11/24.
+//
+
+import SwiftUI
+
+@main
+struct GeeksApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ GeeksTests/GeeksTests.swift
@@ -0,0 +1,16 @@
+//
+//  GeeksTests.swift
+//  GeeksTests
+//
+//  Created by  jwkwon0817 on 11/11/24.
+//
+
+import Testing
+
+struct GeeksTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
